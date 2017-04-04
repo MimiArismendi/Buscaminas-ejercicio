9ea72ee0e6b6e0d33c187730627a32fc0395f066
noemi <noemiarismendi.cc@gmail.com>
2017-04-04 16:45:33 -0500

    buscaminas funciones

---
+++ buscaminas.js
@@ -0,0 +1,9 @@
+function mostrar(){
+document.getElementById('oculto2').style.display = 'block';
+}
+
+function ocultar(){
+document.getElementById("oculto").style.display = 'none';
+/*function mostrarBoton() {
+  document.getElementById("oculto").style.display = "block";*/
+}
